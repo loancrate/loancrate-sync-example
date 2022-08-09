ef343ca59a478a6ea47d3176ce0c9c93fb43ad68
Trevor Robinson <trevor@loancrate.com>
2022-08-09 13:40:13 -0500

    Cleanup fixes

--- src/cleanup.ts
+++ src/cleanup.ts
@@ -5,38 +5,83 @@ export type CleanupHandler = () => void | Promise<void>;
 let cleanupHandlers: Array<CleanupHandler> | undefined;
 
 export function addCleanupHandler(handler: CleanupHandler): void {
+  // Install exit handlers on initial cleanup handler
   if (!cleanupHandlers) {
-    installCleanupHandlers();
+    installExitHandlers();
     cleanupHandlers = [];
   }
+
   cleanupHandlers.push(handler);
 }
 
-function signalHandler(signal: NodeJS.Signals) {
-  logger.info(`Exiting on signal ${signal}`);
+export function exitAfterCleanup(code: number) {
+  executeCleanupHandlers()
+    .then(() => {
+      process.exit(code);
+    })
+    .catch((err) => {
+      logger.error(err, "Unhandled error during cleanup");
+      process.exit(code);
+    });
+}
+
+export function killAfterCleanup(signal: NodeJS.Signals) {
+  executeCleanupHandlers()
+    .then(() => {
+      process.kill(process.pid, signal);
+    })
+    .catch((err) => {
+      logger.error(err, "Unhandled error during cleanup");
+      process.kill(process.pid, signal);
+    });
+}
+
+function executeCleanupHandlers(): Promise<unknown> {
+  // Remove exit handlers in case there are additional signals during cleanup
+  uninstallExitHandlers();
+
+  const promises: Promise<void>[] = [];
   if (cleanupHandlers) {
-    const promises: Promise<void>[] = [];
     for (const handler of cleanupHandlers) {
       const promise = handler();
       if (promise) promises.push(promise);
     }
-    Promise.all(promises)
-      .then(() => {
-        process.kill(process.pid, signal);
-      })
-      .catch((err) => {
-        logger.error(err, "Unhandled error during cleanup");
-        process.kill(process.pid, signal);
-      });
   }
+  return Promise.all(promises);
 }
 
-function installCleanupHandlers() {
-  const exitSignals: NodeJS.Signals[] = [
-    "SIGTERM",
-    "SIGINT",
-    "SIGQUIT",
-    "SIGUSR2",
-  ];
-  exitSignals.forEach((signal) => process.once(signal, signalHandler));
+function beforeExitHandler(code: number) {
+  logger.info(`Exiting with code ${code} due to empty event loop`);
+  exitAfterCleanup(code);
+}
+
+function uncaughtExceptionHandler(error: Error) {
+  logger.error(error, "Exiting with code 1 due to uncaught exception");
+  exitAfterCleanup(1);
+}
+
+function signalHandler(signal: NodeJS.Signals) {
+  logger.info(`Exiting due to signal ${signal}`);
+  killAfterCleanup(signal);
+}
+
+const exitSignals: NodeJS.Signals[] = [
+  "SIGTERM",
+  "SIGINT",
+  "SIGQUIT",
+  "SIGUSR2",
+];
+
+function installExitHandlers() {
+  process.on("beforeExit", beforeExitHandler);
+  process.on("uncaughtException", uncaughtExceptionHandler);
+  exitSignals.forEach((signal) => process.on(signal, signalHandler));
+}
+
+function uninstallExitHandlers() {
+  process.removeListener("beforeExit", beforeExitHandler);
+  process.removeListener("uncaughtException", uncaughtExceptionHandler);
+  exitSignals.forEach((signal) =>
+    process.removeListener(signal, signalHandler)
+  );
 }
